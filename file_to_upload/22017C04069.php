1ad3193909ab4e86b36a1bf4f993d447386682c8
VaibhavBansa1 <vaibhavbansalgwl@gmail.com>
2024-06-19 20:42:47 +0530

    little change

--- file_to_upload/22017C04069.php
+++ file_to_upload/22017C04069.php
@@ -368,8 +368,8 @@ $question_and_file = [
         </div>
 
         <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.05);">
-            <a class="text-body" href="polygwalior.ac.in">Dr. B.R. Ambedkar Polytechnic, College</a> 
-            ©: 2024
+            © 2024 :
+            <a class="text-body" href="https://www.polygwalior.ac.in">Dr. B.R. Ambedkar Polytechnic, College</a> 
         </div>
     </footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
