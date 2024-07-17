9fcf2e7ff00fbe9ced6a6d2b2891bc9aacb64d0e
VaibhavBansa1 <vaibhavbansalgwl@gmail.com>
2024-07-17 14:03:30 +0530

    added file

--- file_to_upload/22017C04069.php
+++ file_to_upload/22017C04069.php
@@ -156,8 +156,8 @@ $question_and_file = [
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>List Of Practical</title>
-    <link rel="shortcut icon" href="https://cdn4.iconfinder.com/data/icons/global-logistics-3/512/104-512.png" type="image/x-icon">
-    <link rel="icon" href="https://cdn4.iconfinder.com/data/icons/global-logistics-3/512/104-512.png" type="image/x-icon">
+    <link rel="shortcut icon" href="https://www.pngwing.com/en/free-png-hoxfb/download" type="image/x-icon">
+    <link rel="icon" href="https://www.pngwing.com/en/free-png-hoxfb/download" type="image/x-icon">
     <!-- <link rel="apple-touch-icon" href="https://cdn4.iconfinder.com/data/icons/global-logistics-3/512/104-512.png"> -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <!-- <script src="https://cdn.jsdelivr.net/npm/@fancyapps/ui@5.0/dist/fancybox/fancybox.umd.js"></script> -->
@@ -396,6 +396,17 @@ $question_and_file = [
 
 
     <script>
+
+        let headTitle = document.head.querySelectorAll("title")[0].innerText;
+        setInterval(()=>{
+            const now = new Date();
+            const hours = now.getHours();
+            const minutes = now.getMinutes();
+            const seconds = now.getSeconds();
+            const formattedTime = `${hours}:${minutes}:${seconds}`;
+            document.head.querySelectorAll("title")[0].innerText = `${headTitle} - ${formattedTime}`;
+        },500);
+
         const closeCross = document.getElementById('close-cross');
         const closeBtn = document.getElementById('close-btn');
         closeCross.addEventListener('click', e => greetWithName(null));
